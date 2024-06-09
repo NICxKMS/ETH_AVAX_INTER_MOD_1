343b03bb14714311a53558397437200519203d5b
Nikhil Kumar <121129363+NICxKMS@users.noreply.github.com>
2024-06-09 12:08:55 +0530

    Update Module1.sol

--- Module1.sol
+++ Module1.sol
@@ -1,38 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Module1{
-    string public name = "Nikhil Kumar";
+contract GameToken {
+    string public name = "GameToken";
     mapping (address => uint256) public balances;
-    uint totalSupply=0;
-    
+    uint256 public totalSupply;
     address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
 
     constructor() {
         owner = msg.sender;
     }
 
-    function mint(address _address,uint _val) public {
-        uint oldTotal=totalSupply;
-        balances[_address]+=_val;
-        totalSupply +=(1+ _val);
-
+    function recharge(address _address, uint256 _val) public onlyOwner {
+        uint256 oldTotal = totalSupply;
+        balances[_address] += _val;
+        totalSupply += _val;
         assert(totalSupply == oldTotal + _val);
     }
 
-    function getTotalSupply() view public  returns (uint256){
-        require(msg.sender == owner , "Only owner can get total supply");
+    function getTotalSupply() view public returns (uint256) {
         return totalSupply;
     }
-    function burn(address _address, uint256 _val) public {
-        require(_val<=balances[_address]);
-        balances[_address]+=_val;
-        totalSupply += _val;
 
+    function redeem(address _address, uint256 _val) public {
+        require(_val <= balances[_address], "Insufficient balance to redeem");
+        uint256 oldTotal = totalSupply;
+        balances[_address] -= _val;
+        totalSupply -= _val;
+        assert(totalSupply == oldTotal - _val);
     }
+
+    function redeemItem(address _address, uint256 _val, string memory _item) public {
+        require(_val <= balances[_address], "Insufficient balance to redeem item");
+        uint256 oldTotal = totalSupply;
+        balances[_address] -= _val;
+        totalSupply -= _val;
+        assert(totalSupply == oldTotal - _val);
+    }
+
     function changeOwner(address _newOwner) public {
-        if(msg.sender != owner) {
-            revert("Requires Owner to personally change to new Owner");
+        if (msg.sender != owner) {
+            revert("Requires owner to personally change to new owner");
         }
         owner = _newOwner;
     }
